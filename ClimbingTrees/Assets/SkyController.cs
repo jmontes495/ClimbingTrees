9f1ed0d1ec21e322fa70c6024c56d1448e45fe8a
Juliana Montes <jmontes@jamcity.com>
2019-08-26 19:53:31 -0500

    Added sky controller to make night appear as you collect fruit

---
+++ ClimbingTrees/Assets/SkyController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyController : MonoBehaviour
+{
+    private Camera skyCamera;
+
+    private float fruitsCollected = 0;
+
+    private float totalFruits;
+
+    private void Awake()
+    {
+        FruitCollector.FruitCollected += IncreaseFruitCounter;
+        FruitCollector.FruitCreated += AddToTotalFruits;
+        skyCamera = GetComponent<Camera>();
+
+    }
+
+    private void AddToTotalFruits()
+    {
+        totalFruits++;
+        AdaptSky();
+    }
+
+    private void IncreaseFruitCounter()
+    {
+        fruitsCollected++;
+        AdaptSky();
+    }
+
+    private void AdaptSky()
+    {
+        if (fruitsCollected >= totalFruits)
+            return;
+
+        skyCamera.backgroundColor = new Color( 0.8f - (fruitsCollected / totalFruits), 1 - (fruitsCollected / totalFruits), 1 - (fruitsCollected / totalFruits)*0.7f, 0);
+    }
+}
