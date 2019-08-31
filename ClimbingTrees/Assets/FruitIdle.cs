32fd0d19db2e4c491f5a08007dce20471aab03d6
Juliana Montes <jmontes@jamcity.com>
2019-08-30 20:32:14 -0500

    Added the sexy mango to the game

--- ClimbingTrees/Assets/FruitIdle.cs
+++ ClimbingTrees/Assets/FruitIdle.cs
@@ -12,20 +12,20 @@ public class FruitIdle : MonoBehaviour
 
     private IEnumerator GoUp()
     {
-        for (float i = 0; i < 0.3f; i += 0.03f)
+        for (float i = 0; i < 0.1f; i += 0.01f)
         {
-            transform.Translate(0, (0.3f - i), 0);
-            yield return new WaitForSeconds(0.05f);
+            transform.Translate(0, (0.1f - i), 0);
+            yield return new WaitForSeconds(0.1f);
         }
         StartCoroutine(GoDown());
     }
 
     private IEnumerator GoDown()
     {
-        for (float i = 0; i < 0.3f; i += 0.03f)
+        for (float i = 0; i < 0.1f; i += 0.01f)
         {
-            transform.Translate(0, -(0.3f - i), 0);
-            yield return new WaitForSeconds(0.05f);
+            transform.Translate(0, -(0.1f - i), 0);
+            yield return new WaitForSeconds(0.1f);
         }
         StartCoroutine(GoUp());
     }
