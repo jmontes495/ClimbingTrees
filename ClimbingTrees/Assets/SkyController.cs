1ca646e5e163adfe702da9cc9166e85022d5a376
Juliana Montes <jmontes@jamcity.com>
2019-09-07 11:42:59 -0500

    Created dynamic skybox

--- ClimbingTrees/Assets/SkyController.cs
+++ ClimbingTrees/Assets/SkyController.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class SkyController : MonoBehaviour
 {
-    private Camera skyCamera;
-
     private float fruitsCollected = 0;
 
     private float totalFruits;
+
+    private float skyboxBlendSpeed = 0.05f;
+
+    private float skyboxBlendFactor = 0;
 
     private void Awake()
     {
         FruitCollector.FruitCollected += IncreaseFruitCounter;
         FruitCollector.FruitCreated += AddToTotalFruits;
-        skyCamera = GetComponent<Camera>();
+        RenderSettings.skybox.SetFloat("_Blend", 0);
+    }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+            IncreaseFruitCounter();
     }
 
     private void AddToTotalFruits()
@@ -35,6 +42,18 @@ public class SkyController : MonoBehaviour
         if (fruitsCollected >= totalFruits)
             return;
 
-        skyCamera.backgroundColor = new Color( 0.8f - (fruitsCollected / totalFruits), 1 - (fruitsCollected / totalFruits), 1 - (fruitsCollected / totalFruits)*0.7f, 0);
+        StartCoroutine(BlendSky());
+    }
+
+    private IEnumerator BlendSky()
+    {
+        float target = (fruitsCollected / totalFruits);
+
+        while (skyboxBlendFactor < target)
+        {
+            skyboxBlendFactor += skyboxBlendSpeed * Time.deltaTime;
+            RenderSettings.skybox.SetFloat("_Blend", skyboxBlendFactor);
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
