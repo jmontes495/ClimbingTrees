41e5e544ae09205e34dced1039781986af23d49a
Juliana Montes <jmontes@jamcity.com>
2019-11-15 08:28:50 -0500

    Added noise and petals to flowers

---
+++ ClimbingTrees/Assets/Scripts/FlowerContact.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerContact : MonoBehaviour
+{
+    public delegate void PetalActions();
+    public static event PetalActions FlowerCollided;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name != "Player")
+            return;
+
+        FlowerCollided();
+        audioSource.Play();
+    }
+}
---
+++ ClimbingTrees/Assets/Scripts/PetalsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetalsController : MonoBehaviour
+{
+    [SerializeField]
+    private PetalsAnimation[] slots;
+
+    private void Start()
+    {
+        slots = GetComponentsInChildren<PetalsAnimation>();
+        FlowerContact.FlowerCollided += ShowPetals;
+    }
+
+    private void ShowPetals()
+    {
+        foreach (PetalsAnimation slot in slots)
+        {
+            if (!slot.IsRunning)
+            {
+                slot.BeginPetals();
+                break;
+            }
+        }
+    }
+}
